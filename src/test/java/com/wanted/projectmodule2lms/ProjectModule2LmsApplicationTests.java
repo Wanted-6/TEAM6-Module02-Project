14dca774be3adb81ef7858965e1ac3d1b53f3bd3
BJH12-byte <qowjdgus0629@g.eulji.ac.kr>
2026-04-02 18:21:15 +0900

    first commit

---
+++ src/test/java/com/wanted/projectmodule2lms/ProjectModule2LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.projectmodule2lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectModule2LmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
