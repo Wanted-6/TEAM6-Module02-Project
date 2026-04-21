b60d0e963972aa2ceb251450d941f5618fd0f030
mac0616 <mac0616@naver.com>
2026-04-21 17:43:17 +0900

    코드수정

--- src/main/java/com/wanted/projectmodule2lms/global/util/SecurityUtil.java
+++ src/main/java/com/wanted/projectmodule2lms/global/util/SecurityUtil.java
@@ -9,12 +9,6 @@ public class SecurityUtil {
 
     private SecurityUtil() {}
 
-//    View 에서 Session 에 저장 된 회원의 PK 를 hidden input 같은 걸로 넘기면,
-//    사용자가 값을 바꿔서 다른 사람 ID로 글을 등록하는 것처럼 조작할 수 있다.
-//    반면 서버에서 현재 로그인한 사용자를 기준으로 작성자를 정하면, 클라이언트가 작성자 정보를 마음대로 바꿀 수 없다.
-//    @AuthenticationPrincipal AuthDetails authDetails 를 활용해서 로그인 한 사용자의 정보를 꺼내보자.
-// 회원의 pk가 필요할 때 이거 사용해서 꺼내써야 함.
-
     public static Long getCurrentMemberId() {
         // 시큐리티 컨텍스트에서 현재 인증 정보(Authentication) 가져옴.
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -37,17 +31,6 @@ public class SecurityUtil {
     }
 }
 
-/* 사용방법
-*  Service, Controller 에서 호출할 때
-
-Long currentMemberId = SecurityUtil.getCurrentMemberId();
-
-if (currentMemberId != null) {
-    System.out.println("현재 로그인한 사용자 PK: " + currentMemberId);
-} else {
-    System.out.println("로그인한 사용자 없음");
-}
-* */
 
 
 
